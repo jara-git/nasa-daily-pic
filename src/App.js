bce6905ef965680d7d66b88a940d8cea733f97b9
solar <jarabelmonte@gmail.com>
2020-07-30 22:38:35 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import NasaPic from "./components/NasaPic";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      setting up
+    <div>
+      <Route component={Home} path="/" exact/>
+      <Route component={NasaPic} path="/nasa-picture" />
     </div>
   );
 }
